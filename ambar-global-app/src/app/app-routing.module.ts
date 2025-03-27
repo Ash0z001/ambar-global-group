4b74591e62dfdaa9acebd97a863cab754b97a4c4
Ashley Govender <ashdevelop01@gmail.com>
2025-03-27 12:45:08 +0200

    removed additional code

--- ambar-global-app/src/app/app-routing.module.ts
+++ ambar-global-app/src/app/app-routing.module.ts
@@ -1,27 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { GalleryComponent } from './pages/gallery/gallery.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NewsComponent } from './pages/news/news.component';
-import { OccupationalTherapyComponent } from './pages/occupational-therapy/occupational-therapy.component';
-import { SpeechAndHearingTherapyComponent } from './pages/speech-and-hearing-therapy/speech-and-hearing-therapy.component';
-import { TherapyResourcesComponent } from './pages/therapy-resources/therapy-resources.component';
-import { TraumaCouncillingComponent } from './pages/trauma-councilling/trauma-councilling.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  // { path: 'articles', component: NewsComponent },
-  { path: 'occupational-therapy', component: OccupationalTherapyComponent },
-  { path: 'trauma-counselling', component: TraumaCouncillingComponent },
-  // { path: 'speech-and-hearing', component: SpeechAndHearingTherapyComponent },
-  { path: 'gallery', component: GalleryComponent },
-  { path: 'therapy-resources', component: TherapyResourcesComponent },
-  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
