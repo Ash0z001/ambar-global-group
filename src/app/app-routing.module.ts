40af8470eba34895853b556911a23bf9dc1ace5a
Yashveer Sewparthab <yashveersewparthab@gmail.com>
2025-03-28 19:11:19 +0200

    routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: HomeComponent },  // Wildcard route for a 404 page
 ];
