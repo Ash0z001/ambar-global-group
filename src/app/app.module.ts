354dde37e631ef1550850ea7a42849be8d432f71
Yashveer Sewparthab <yashveersewparthab@gmail.com>
2025-03-28 16:23:54 +0200

    firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireAnalyticsModule } from "@angular/fire/compat/analytics";
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
@@ -8,6 +14,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,7 +27,8 @@ import { ServicesComponent } from './pages/services/services.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCUQ1nYbDGhoZ9Erx_o8UN7mXhddfypQeU",
+    authDomain: "anime-verse-online.firebaseapp.com",
+    projectId: "anime-verse-online",
+    storageBucket: "anime-verse-online.appspot.com",
+    messagingSenderId: "319801350599",
+    appId: "1:319801350599:web:d911d2acfef86d65a8a1e2",
+    databaseURL: "https://anime-verse-online-default-rtdb.europe-west1.firebasedatabase.app",
+    measurementId: "G-CYBY83KCYP"
+  }
+}
